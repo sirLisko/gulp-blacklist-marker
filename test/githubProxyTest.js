637cf8361807808733c2c04724500881b09ccdce
Luca Lischetti <sirlisko@gmail.com>
2015-07-28 21:12:45 +0100

    moved tests to tape

--- test/githubProxyTest.js
+++ test/githubProxyTest.js
@@ -1,16 +1,14 @@
 'use strict';
 
-var assert = require('assert');
+var test = require('tape');
 var request = require('request');
 
 var blackListUrl = 'https://github-raw-cors-proxy.herokuapp.com/gulpjs/plugins/master/src/blackList.json';
 
-describe('github proxy cors for raw files', function(done){
-	it('is working properly', function(){
-		request(blackListUrl, function (error, response) {
-			assert.equal(response.statusCode, 200, 'external service is working');
-			assert.equal(typeof JSON.parse(response.body), 'object', 'service is returning a JSON');
-			done();
-		});
+test('github proxy cors for raw files is working properly', function(t){
+	request(blackListUrl, function (error, response) {
+		t.equal(response.statusCode, 200, 'external service is working');
+		t.equal(typeof JSON.parse(response.body), 'object', 'service is returning a JSON');
+		t.end();
 	});
 });
