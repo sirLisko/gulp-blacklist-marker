9290eb4b802707a1dec6db9012f510d8a4a0e05d
Luca Lischetti <luca@sirlisko.com>
2020-01-08 14:55:35 +0100

    added trim to cope with the module check

--- test/thirdPartyDOM.spec.js
+++ test/thirdPartyDOM.spec.js
@@ -1,72 +1,75 @@
 /* eslint handle-callback-err: 0 */
 
-'use strict'
+"use strict";
 
-var test = require('tape')
-var request = require('request')
-var cheerio = require('cheerio')
+var test = require("tape");
+var request = require("request");
+var cheerio = require("cheerio");
 
-var SELECTORS = require('../src/services').SELECTORS
+var SELECTORS = require("../src/services").SELECTORS;
 
-var NPMPackageURL = 'https://www.npmjs.com/package/gulp-clean'
-var NPMSearchURL = 'https://www.npmjs.com/search?q=gulp-clean'
+var NPMPackageURL = "https://www.npmjs.com/package/gulp-clean";
+var NPMSearchURL = "https://www.npmjs.com/search?q=gulp-clean";
 var GithubPackageURL =
-  'https://github.com/sirLisko/sirlisko.com/blob/5c88c52cab8d7dd9768ebfcc2d7cd21790f99865/package.json'
-var GithubModuleURL = 'https://github.com/robrich/gulp-rimraf'
+  "https://github.com/sirLisko/sirlisko.com/blob/5c88c52cab8d7dd9768ebfcc2d7cd21790f99865/package.json";
+var GithubModuleURL = "https://github.com/robrich/gulp-rimraf";
 
-test('NPM Package DOM is still correct', function (t) {
-  request(NPMPackageURL, function (error, response) {
-    const $ = cheerio.load(response.body)
+test("NPM Package DOM is still correct", function(t) {
+  request(NPMPackageURL, function(error, response) {
+    const $ = cheerio.load(response.body);
     t.equal(
       $(SELECTORS.npmPackage)
         .first()
-        .text(),
-      'gulp-clean',
-      'module name found'
-    )
-    t.equal($(SELECTORS.npmPackage).length, 2, 'there are two elements')
-    t.end()
-  })
-})
+        .text()
+        .trim(),
+      "gulp-clean",
+      "module name found"
+    );
+    t.equal($(SELECTORS.npmPackage).length, 2, "there are two elements");
+    t.end();
+  });
+});
 
-test('NPM Search DOM is still correct', function (t) {
-  request(NPMSearchURL, function (error, response) {
-    const $ = cheerio.load(response.body)
+test("NPM Search DOM is still correct", function(t) {
+  request(NPMSearchURL, function(error, response) {
+    const $ = cheerio.load(response.body);
     t.equal(
       $(SELECTORS.npm)
         .first()
-        .text(),
-      'gulp-clean',
-      'module name found'
-    )
-    t.end()
-  })
-})
+        .text()
+        .trim(),
+      "gulp-clean",
+      "module name found"
+    );
+    t.end();
+  });
+});
 
-test('Github Package DOM is still correct', function (t) {
-  request(GithubPackageURL, function (error, response) {
-    const $ = cheerio.load(response.body)
+test("Github Package DOM is still correct", function(t) {
+  request(GithubPackageURL, function(error, response) {
+    const $ = cheerio.load(response.body);
     t.equal(
       $(SELECTORS.githubPackage).filter(
         (i, module) => $(module).text() === '"gulp-rimraf"'
       ).length,
       1,
-      'module name found'
-    )
-    t.end()
-  })
-})
+      "module name found"
+    );
+    t.end();
+  });
+});
 
-test('Github Module DOM is still correct', function (t) {
-  request(GithubModuleURL, function (error, response) {
-    const $ = cheerio.load(response.body)
+test("Github Module DOM is still correct", function(t) {
+  request(GithubModuleURL, function(error, response) {
+    const $ = cheerio.load(response.body);
     t.equal(
       $(SELECTORS.github)
         .first()
-        .text(),
-      'gulp-rimraf',
-      'module name found'
-    )
-    t.end()
-  })
-})
+        .text()
+        .trim(),
+      "gulp-rimraf",
+      "module name found"
+    );
+    t.end();
+  });
+});
